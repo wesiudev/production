a4ebf3ed0ef5585199cf6e00d0eda8ba02ed1e65
wesiudev <wesselpawel@wp.pl>
2023-05-23 17:09:22 +0200

    fix

--- app/browse/components/ImagePreview.tsx
+++ app/browse/components/ImagePreview.tsx
@@ -4,13 +4,14 @@ import { useSelector } from "react-redux";
 import { Layout } from "./dom/Layout";
 import { RootState } from "@/common/redux/store";
 import Canvas3D from "../components/Canvas3D";
+import { useState } from "react";
 export const ImagePreview = () => {
   const { currentOpen } = useSelector((state: RootState) => state.images);
   return (
     <>
       {currentOpen ? (
-        <div className="fixed left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] h-[90vh] sm:h-[80vh] rounded-lg z-50">
-          <div className="w-[90vw] lg:w-[80vw] h-[30vh] overflow-y-scroll scrollbar">
+        <div className="fixed left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] h-[95vh] sm:h-[80vh] rounded-lg z-50">
+          <div className="w-[90vw] lg:w-[80vw] h-[50vh] overflow-y-scroll scrollbarBlack">
             <Layout>
               <Canvas3D image={currentOpen} />
             </Layout>
