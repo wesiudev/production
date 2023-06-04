6b7532e1117871d9c34a6e176cb552bd7a54a681
wesiudev <wesselpawel@wp.pl>
2023-06-04 18:21:47 +0200

    shop infinite scroll

--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -3,7 +3,7 @@ import SSRImageGrid from "./components/SSRImagesGrid";
 import { setBrowseImages } from "@/common/redux/slices/imagesSlice";
 
 export default async function BrowseImages() {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/images`, {
+  const req = await fetch(`https://www.decocanva.com/api/images`, {
     headers: {
       authorization: process.env.API_KEY!,
     },
